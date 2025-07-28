dfe905d14b20e9637a4d04641f6a9beb94dc0281
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2025-07-28 20:36:12 +0530

    Revert "full app"
    This reverts commit b6e7c75cd2d7dd23bd6c327e0ae507477628942f.

--- Program.cs
+++ Program.cs
@@ -75,22 +75,9 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(key)
 
-
+        
     };
 });
-
-// Add CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
-
 
 // =============================
 // 5. Authorization (for Role-based access)
@@ -113,9 +100,6 @@ app.UseRouting();
 // Enable JWT Authentication & Authorization
 app.UseAuthentication();    // Must come before UseAuthorization
 app.UseAuthorization();
-
-// Use CORS
-app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
